b404936efc015a9aeb84775bac04cf4effca18d9
OmniDvW <geoffrey.frioli@gmail.com>
2023-06-03 08:03:53 +0200

    start

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const About = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>a propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod officiis nam vitae labore, rerum soluta placeat ad, tempora nulla nisi nobis omnis! Mollitia omnis obcaecati a eveniet? Perferendis, ipsa repellendus.</p>
+        </div>
+    );
+};
+
+export default About;
