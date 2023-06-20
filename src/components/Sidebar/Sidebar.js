3081df300e962eafae61c397c8901488226317ee
OmniDvW <geoffrey.frioli@gmail.com>
2023-06-20 12:45:23 +0200

    update

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { ApiContext } from '../../context/apiContext';
 import "./Sidebar.scss"
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,43 @@
-import React from 'react';
-// import { ApiContext } from '../../context/apiContext';
+import React, { useEffect, useContext, useState } from 'react';
+import { ApiContext } from '../../context/apiContext';
 import "./Home.scss";
 
 
 
 
 const Home = () => {
+    const { apiDataFixtures } = useContext(ApiContext);
+    const [matchesByLeague, setMatchesByLeague] = useState({});
 
+    useEffect(() => {
+        const matchesByLeague = {};
+        apiDataFixtures.forEach(match => {
+            const leagueId = match.league.id;
+            if (matchesByLeague[leagueId]) {
+                matchesByLeague[leagueId].push(match);
+            } else {
+                matchesByLeague[leagueId] = [match];
+            }
+        });
+        setMatchesByLeague(matchesByLeague);
+    }, [apiDataFixtures]);
 
     return (
         <div className='home'>
-            <div className="home_container">
-                <div className="home_container_content">
-                    <div className="info_card">
-                        <h1>hello home</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sed quo voluptatibus facilis officia sit! Officiis neque consectetur dolores cum molestias nam saepe corporis dolore temporibus earum tempora minima, voluptates nemo. In doloremque enim recusandae mollitia maiores suscipit laboriosam, beatae temporibus quasi obcaecati ducimus aliquam, ratione eum tempore commodi error, sed quas reprehenderit nam. Rerum earum sapiente natus praesentium aut, possimus, nisi, quam alias rem tempore expedita exercitationem fugit iusto distinctio officia facilis cumque assumenda obcaecati? Adipisci sequi itaque harum, vitae numquam aliquid tenetur soluta quod deserunt. Nihil laudantium, asperiores, eveniet voluptates magnam laboriosam deleniti animi nemo aliquam incidunt vel illum, necessitatibus quasi illo iste quaerat! Amet aut, rerum repellendus optio voluptatem accusamus. Recusandae velit nisi hic unde non cum inventore quaerat, cupiditate molestias laboriosam corrupti odit provident illo. Sequi iusto consequuntur, alias non nulla earum reprehenderit voluptatem veritatis debitis aperiam? Voluptatibus praesentium expedita odit adipisci culpa aut est minima esse omnis laudantium, saepe perspiciatis consequatur aperiam facilis velit sit rerum accusantium molestias atque maxime veniam magni rem in. At, magni possimus nam neque id ea, sequi impedit provident dignissimos iusto quae sint molestias quis mollitia fugiat cumque sit alias. Quidem libero explicabo dolor eveniet consectetur cum et. Ex qui debitis quae, sit amet vero neque, veniam vitae sint temporibus dicta consequatur. Quod voluptate alias illo hic veritatis maxime cupiditate, qui nesciunt consectetur ex asperiores tempore eligendi quae beatae, repudiandae dignissimos. Dolor provident laudantium obcaecati totam, aperiam doloremque perspiciatis fuga blanditiis at consequatur quo quae atque! Vero odio iure eaque.</p>
-                    </div>
+            <div className='home-header'>
+
+            </div>
+            <div className="home-container">
+                <div className='home-container-content'>
+                    {Object.keys(matchesByLeague).map(leagueId => (
+                        <div key={leagueId} className='card-match'>
+                            {matchesByLeague[leagueId].map(match => (
+                                <div key={match.fixture.id} className='match'>
+                                    {match.fixture.id} vs {match.fixture.id}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
