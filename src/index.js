368fc1e8d2603469bd52c4fff5016ddaee0688f6
OmniDvW <geoffrey.frioli@gmail.com>
2023-06-23 18:49:35 +0200

    update to deploy

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode> désactivé pour évité un double call api avec useEffect
-  <ApiProvider>
-    <App />
-  </ApiProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ApiProvider>
+      <App />
+    </ApiProvider>
+  </React.StrictMode>
 );
 
